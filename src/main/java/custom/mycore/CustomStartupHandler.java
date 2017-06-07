b307bfd4dc9357e494d2c2a6da02c45989787fb3
Paul Rochowski <Paul.Rochowski@uni-due.de>
2017-06-07 09:03:49 +0200

    # simple startup handler example for extension reason

---
+++ src/main/java/custom/mycore/CustomStartupHandler.java
@@ -0,0 +1,44 @@
+package custom.mycore;
+
+import javax.servlet.ServletContext;
+
+import org.apache.log4j.Logger;
+import org.mycore.common.events.MCRStartupHandler.AutoExecutable;
+
+/**
+ *
+ * Simple Startup Handler example for extension reason
+ * 
+ * To integrate this StartupHandler into startup process, use package +
+ * classname for MCR.Startup.Class property in mycore.properties (MyCoRe home
+ * directory)
+ * 
+ * MCR.Startup.Class=custom.mycore.CustomStartupHandler,...
+ *
+ */
+public class CustomStartupHandler implements AutoExecutable {
+
+	private static final String HANDLER_NAME = CustomStartupHandler.class.getName();
+
+	private static final Logger LOGGER = Logger.getLogger(CustomStartupHandler.class);
+
+	@Override
+	public String getName() {
+		return HANDLER_NAME;
+	}
+
+	@Override
+	public int getPriority() {
+
+		return 0;
+	}
+
+	@Override
+	public void startUp(ServletContext servletContext) {
+
+		if (servletContext != null) {
+
+			LOGGER.info("Startup custom");
+		}
+	}
+}
